0447e3a3f9bf985efdaf58b97aa3b93eea785efd
Prathamesh Ajay Deshmukh <deshmukhprathamesh4141@gmail.com>
2025-02-08 12:40:08 +0530

    Your commit message here

---
+++ catagory_details.js
@@ -0,0 +1,411 @@
+[
+    {
+      id : 0,
+      short_title: "Agriculture Equipment Batteries",
+      short_description:
+        "These batteries are designed to power agricultural machinery such as tractors, harvesters, and irrigation systems. They offer high performance and durability for extended outdoor use.",
+      short_image: modalEmployeeGroup, // Replace with actual image URL
+      title : "Agriculture Equipment Batteries",
+      description : "These batteries are designed to power agricultural machinery such as tractors, harvesters, and irrigation systems.",
+      image : CycleProduct,
+      Batteries : [ "679752a55188016a95c85058","6794c19169553d0ef902eb11","679340a107650594edbcd1c1","679752a55188016a95c85058","6794c19169553d0ef902eb11","679340a107650594edbcd1c1","679752a55188016a95c85058","6794c19169553d0ef902eb11",],
+
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    {
+      id : 1,
+      short_title: "E-bikes & Two-Wheel Vehicles",
+      short_description: "Batteries for electric bikes and scooters, providing eco-friendly, rechargeable energy storage.",
+      short_image: AgricultureEquipmentBatteries, // Replace with actual image URL
+      title : "E-BIKE",
+      description : "Go the Distance Lithium-ion battery packs for an eBicycle (e-Bike)",
+      image : CycleProduct,
+      Batteries : [,"679752a55188016a95c85059"],
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    {
+      id : 2,
+      short_title: "Three-Wheel & Four-Wheel Vehicles",
+      short_description:
+        "These batteries are used in three-wheelers and four-wheelers like small electric cars, offering reliable power for urban transport and cargo.",
+      short_image: BatteryManufacturingExcellence, // Replace with actual image URL
+      title : "Three-Wheel & Four-Wheel Vehicles",
+      description : "Go the Distance Lithium-ion battery packs for an eBicycle (e-Bike)",
+      image : CycleProduct,
+      Batteries : [0,1,2],
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    {
+      id : 0,
+      short_title: "Agriculture Equipment Batteries",
+      short_description:
+        "These batteries are designed to power agricultural machinery such as tractors, harvesters, and irrigation systems. They offer high performance and durability for extended outdoor use.",
+      short_image: modalEmployeeGroup, // Replace with actual image URL
+      title : "Agriculture Equipment Batteries",
+      description : "These batteries are designed to power agricultural machinery such as tractors, harvesters, and irrigation systems.",
+      image : CycleProduct,
+      Batteries : [0,1,2],
+
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    {
+      id : 1,
+      short_title: "E-bikes & Two-Wheel Vehicles",
+      short_description: "Batteries for electric bikes and scooters, providing eco-friendly, rechargeable energy storage.",
+      short_image: AgricultureEquipmentBatteries, // Replace with actual image URL
+      title : "E-BIKE",
+      description : "Go the Distance Lithium-ion battery packs for an eBicycle (e-Bike)",
+      image : CycleProduct,
+      Batteries : [0,1,2],
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    {
+      id : 2,
+      short_title: "Three-Wheel & Four-Wheel Vehicles",
+      short_description:
+        "These batteries are used in three-wheelers and four-wheelers like small electric cars, offering reliable power for urban transport and cargo.",
+      short_image: BatteryManufacturingExcellence, // Replace with actual image URL
+      title : "Three-Wheel & Four-Wheel Vehicles",
+      description : "Go the Distance Lithium-ion battery packs for an eBicycle (e-Bike)",
+      image : CycleProduct,
+      Batteries : [0,1,2],
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    {
+      id : 0,
+      short_title: "Agriculture Equipment Batteries",
+      short_description:
+        "These batteries are designed to power agricultural machinery such as tractors, harvesters, and irrigation systems. They offer high performance and durability for extended outdoor use.",
+      short_image: modalEmployeeGroup, // Replace with actual image URL
+      title : "Agriculture Equipment Batteries",
+      description : "These batteries are designed to power agricultural machinery such as tractors, harvesters, and irrigation systems.",
+      image : CycleProduct,
+      Batteries : [0,1,2],
+
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    {
+      id : 1,
+      short_title: "E-bikes & Two-Wheel Vehicles",
+      short_description: "Batteries for electric bikes and scooters, providing eco-friendly, rechargeable energy storage.",
+      short_image: AgricultureEquipmentBatteries, // Replace with actual image URL
+      title : "E-BIKE",
+      description : "Go the Distance Lithium-ion battery packs for an eBicycle (e-Bike)",
+      image : CycleProduct,
+      Batteries : [0,1,2],
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    {
+      id : 2,
+      short_title: "Three-Wheel & Four-Wheel Vehicles",
+      short_description:
+        "These batteries are used in three-wheelers and four-wheelers like small electric cars, offering reliable power for urban transport and cargo.",
+      short_image: BatteryManufacturingExcellence, // Replace with actual image URL
+      title : "Three-Wheel & Four-Wheel Vehicles",
+      description : "Go the Distance Lithium-ion battery packs for an eBicycle (e-Bike)",
+      image : CycleProduct,
+      Batteries : [0,1,2],
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    {
+      id : 0,
+      short_title: "Agriculture Equipment Batteries",
+      short_description:
+        "These batteries are designed to power agricultural machinery such as tractors, harvesters, and irrigation systems. They offer high performance and durability for extended outdoor use.",
+      short_image: modalEmployeeGroup, // Replace with actual image URL
+      title : "Agriculture Equipment Batteries",
+      description : "These batteries are designed to power agricultural machinery such as tractors, harvesters, and irrigation systems.",
+      image : CycleProduct,
+      Batteries : [0,1,2],
+
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    {
+      id : 1,
+      short_title: "E-bikes & Two-Wheel Vehicles",
+      short_description: "Batteries for electric bikes and scooters, providing eco-friendly, rechargeable energy storage.",
+      short_image: AgricultureEquipmentBatteries, // Replace with actual image URL
+      title : "E-BIKE",
+      description : "Go the Distance Lithium-ion battery packs for an eBicycle (e-Bike)",
+      image : CycleProduct,
+      Batteries : [0,1,2],
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    {
+      id : 2,
+      short_title: "Three-Wheel & Four-Wheel Vehicles",
+      short_description:
+        "These batteries are used in three-wheelers and four-wheelers like small electric cars, offering reliable power for urban transport and cargo.",
+      short_image: BatteryManufacturingExcellence, // Replace with actual image URL
+      title : "Three-Wheel & Four-Wheel Vehicles",
+      description : "Go the Distance Lithium-ion battery packs for an eBicycle (e-Bike)",
+      image : CycleProduct,
+      Batteries : [0,1,2],
+    features : [
+      {
+        description : "Highly Reliable Source Of Power",
+        image : efficient,
+
+      },
+      {
+        description : "Stringent Durability Testing",
+        image : high_durability,
+
+      },
+      {
+        description :"Efficient and Compact Design",
+        image : low_running_cost,
+
+      },
+      {
+        description : "Competitive Pricing",
+        image : quick_charge,
+
+      },
+
+    ]
+    },
+    
+  ]
